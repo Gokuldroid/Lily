762aca74870c58c0eeca01ec734cfd3feb0ed75e
Gokuldroid <tuple.me@gmail.com>
2017-10-07 23:34:53 +0530

    jit pack issue

--- app/src/main/java/me/tuple/lily/example/MainActivity.kt
+++ app/src/main/java/me/tuple/lily/example/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
-import me.tuple.lily.core.Contexter
 import me.tuple.lily.core.bind
 import me.tuple.lily.utils.intPreference
 import java.util.*
@@ -12,19 +11,19 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
 
+    val button by bind<Button>(R.id.click)
     var deom by intPreference("Gokul")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button by bind<Button>(R.id.click)
         var inn by intPreference("Hello")
-        button.setOnClickListener({
-            Log.d("D","Inn ::" +inn)
+        button.setOnClickListener {
+            Log.d("D", "Inn ::" + inn)
             inn = Random().nextInt(20)
-            Log.d("D","demo ::" +deom)
+            Log.d("D", "demo ::" + deom)
             deom++
-        })
+        }
     }
 
 }
