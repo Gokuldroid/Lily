3893babe04d384d36f3525a5322393f5e710a85b
Gokuldroid <tuple.me@gmail.com>
2017-10-28 15:14:58 +0530

    jit pack addded for lily adapeter

--- app/src/main/java/me/tuple/lily/example/MainActivity.kt
+++ app/src/main/java/me/tuple/lily/example/MainActivity.kt
@@ -1,6 +1,8 @@
 package me.tuple.lily.example
 
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.IntentCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import me.tuple.lily.core.*
