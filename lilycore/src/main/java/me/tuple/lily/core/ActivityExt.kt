279c2d59b23f2c89a72362484792eb5bc3aeb200
gokuldroid <gokulprabhu.droid@gmail.com>
2018-07-29 02:17:41 +0530

    Added more functions

--- lilycore/src/main/java/me/tuple/lily/core/ActivityExt.kt
+++ lilycore/src/main/java/me/tuple/lily/core/ActivityExt.kt
@@ -3,7 +3,9 @@ package me.tuple.lily.core
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.support.annotation.IdRes
 import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 
 /**
@@ -19,13 +21,16 @@ fun Activity.sendMail(chooserTitle: String, email: String, subject: String, body
 }
 
 inline fun AppCompatActivity.supportFragmentTransaction(receiver: FragmentTransaction.() -> Unit) {
-    val manager = supportFragmentManager.beginTransaction()
-    receiver.invoke(manager)
-    manager.commit()
+    supportFragmentManager.beginTransaction().apply(receiver).commit()
 }
 
-inline  fun AppCompatActivity.fragmentTransaction(receiver: android.app.FragmentTransaction.() -> Unit) {
-    val manager = fragmentManager.beginTransaction()
-    receiver.invoke(manager)
-    manager.commit()
+inline fun AppCompatActivity.fragmentTransaction(receiver: android.app.FragmentTransaction.() -> Unit) {
+    fragmentManager.beginTransaction().apply(receiver).commit()
+}
+
+inline fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.() -> Unit) {
+    setSupportActionBar(findViewById(toolbarId))
+    supportActionBar?.run {
+        action()
+    }
 }
