e90c1ac87f6dd6a8f373291f4a4aa4cb39f577f3
gokuldroid <gokulprabhu.droid@gmail.com>
2018-08-09 00:49:17 +0530

    Shared prefs issue fix

--- app/src/main/java/me/tuple/lily/example/MainActivity.kt
+++ app/src/main/java/me/tuple/lily/example/MainActivity.kt
@@ -2,32 +2,25 @@ package me.tuple.lily.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import me.tuple.lily.core.async
 import me.tuple.lily.core.bind
 import me.tuple.lily.core.runOnUI
 import me.tuple.lily.core.safeExecute
+import me.tuple.lily.utils.booleanPreference
 import me.tuple.lily.utils.intPreference
 
 class MainActivity : AppCompatActivity() {
 
 
     val button by bind<Button>(R.id.click)
-    var deom by intPreference("Gokul")
+    var deom by booleanPreference("Gokul")
     var num = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var inn by intPreference("Hello")
-        lifecycle
-        async {
-            val result = safeExecute<String> { "Gokul" }
-            runOnUI {
-                if (result.isSuccessful) {
-
-                }
-            }
-        }
+        Log.d("Main", "" + deom)
     }
 }
